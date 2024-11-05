dee1c63fd58890f54e5cf9142a75297f19b06b56
khemraj <khemrajraj@73gmail.com>
2024-11-05 22:25:56 +0530

    Create dummy users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,16 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'User',
+            'email' => 'user@gmail.com',
+        ]);
+        User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@gmail.com',
+        ]);
+        User::factory()->create([
+            'name' => 'Test',
+            'email' => 'test@gmail.com',
         ]);
     }
 }
